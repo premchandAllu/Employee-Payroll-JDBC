7ecb8771a5d5bd000eccd405f466d26ab8d3d080
premchandAllu <premchandallu1999@gmail.com>
2020-11-02 10:33:23 +0530

    Added: Ability to create watch service

---
+++ test/java/com/blz/employeePayrollJDBC/EmployeePayrollServiceTest.java
@@ -0,0 +1,30 @@
+package com.blz.employeePayrollJDBC;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.blz.employeePayrollJDBC.EmployeePayrollService.IOService;
+
+public class EmployeePayrollServiceTest {
+	@Test
+	public void given3EmployeeWhenWrittenToFileShouldMatchEmployeeEntries() {
+		EmployeePayrollData[] arrayOfEmps = { new EmployeePayrollData(1, "Jeff Bezos", 100000.0),
+				new EmployeePayrollData(2, "Bill Gates", 200000.0),
+				new EmployeePayrollData(3, "Mark Zuckerberg", 300000.0) };
+		EmployeePayrollService employeePayrollService;
+		employeePayrollService = new EmployeePayrollService(Arrays.asList(arrayOfEmps));
+		employeePayrollService.writeEmployeePayrollData(IOService.FILE_IO);
+		employeePayrollService.printData(IOService.FILE_IO);
+		long entries = employeePayrollService.countEntries(IOService.FILE_IO);
+		Assert.assertEquals(3, entries);
+	}
+
+	@Test
+	public void givenFileOnReadingFileShouldMatchEmployeeCount() {
+		EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+		List<EmployeePayrollData> entries = employeePayrollService.readPayrollData(IOService.FILE_IO);
+	}
+}
